3b5c1cd18a8fdd57ff2cad8e3d8af5e3861a3e59
Olivier Hofman <oli.hofman@gmail.com>
2022-11-02 14:35:29 +0000

    More normalization

--- normalize.sh
+++ normalize.sh
@@ -7,11 +7,18 @@ gunzip data_raw/*/*.gz
 echo "Add xml extension to all files that didn't have it"
 ls data_raw/*/* | grep -v 'xml$' | xargs -I %  mv % %.xml
 
-echo "Rename 'Stores...' files to 'StoresFull'"
+echo "Rename 'Stores...' files to 'StoresFull...'"
 ls data_raw/*/* | grep -v '/StoresFull' | grep 'Stores' | xargs -I % echo mv % % | sed 's/Stores/StoresFull/2' | bash
+
+# The numbers are chain, store, date, and ???
+echo "Remove last set of digits from stores with 4 set of digits."
+rename 's/([a-zA-Z]+\d+-\d+-\d+)-\d+.xml/$1.xml/' data_raw/*/*
 
 echo "Remove date from the filename."
 rename 's/([a-zA-Z]+\d+-\d+)-\d+.xml/$1.xml/' data_raw/*/*
+
+echo "Remove date from the filename when there is no store id."
+rename 's/([a-zA-Z]+\d+)--\d+.xml/$1.xml/' data_raw/*/*
 
 echo "Convert all files to utf-8"
 for f in data_raw/*/* 
