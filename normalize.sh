d72d0c7b8956333e4571f3e3cf185eabbf729e90
Olivier Hofman <oli.hofman@gmail.com>
2022-11-06 08:43:40 +0000

    Fixes the encoding conversion.
    some files were detected as being latin-1 (iso-8859-1)
    instead of hebrew (iso-8859-8)

--- normalize.sh
+++ normalize.sh
@@ -12,8 +12,11 @@ for f in data_raw/*/*
 do
     charset=`file -i $f | cut -d"=" -f2`
     if [ "$charset" != "utf-8" ]; then
-        if [ "$charset" == "unknown-8bit" ]; then
-            charset=`head -n 1 $f | cut -d'"' -f4`
+        # In both of those cases, the file tool returns a bad charset.
+        # Happily, in those cases, the xml file has the encoding :)
+        # iso-8859-1 is latin-1, while the file is actually iso-8859-8, hebrew.
+        if [ "$charset" = "unknown-8bit" ] || [ "$charset" = "iso-8859-1" ]; then
+            charset=`head -n 1 $f | sed 's/.*encoding="//' | cut -d'"' -f1`
         fi
         echo "Converting $f from $charset to utf-8"
         iconv -f "$charset" -t utf8 -o "$f.new" "$f"  
