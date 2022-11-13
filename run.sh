5e0a33b847d47fd920ac3b9f1ac5a281f654be06
Olivier Hofman <oli.hofman@gmail.com>
2022-11-13 18:03:18 +0000

    Add pushing to git as part of run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 sh download_all.sh
 sh normalize.sh
-sh xml_to_json.sh
+sh xml_to_csv.sh
+if [ ! -d "israel-prices-data" ];
+then
+    git clone https://github.com/OlivierH/israel-prices-data.git
+fi
+cp data_csv/* israel-prices-data -r
+cd israel-prices-data
+git tag "`date +%F`"
+git commit -a -m"Data for `date +%F`"
+git push
