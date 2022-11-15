d8d9201043bc6acb9664268324899dc3fe22cfdc
Olivier Hofman <oli.hofman@gmail.com>
2022-11-15 09:43:45 +0000

    Check assumption about internal products

---
+++ assumptions.md
@@ -0,0 +1,3 @@
+This document resumes some assumptions that were made about the data.
+
+1. Internal products barcodes are unique in a chain. If two internal products in different stores of the same chain have the same barcode, then they are the same product.
---
+++ scripts/analyze_internal_products.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+data = set()
+
+for root, dirs, files in os.walk("data_csv/prices"):
+    for name in files:
+        filename = os.path.join(root, name)
+        # print("> ", filename)
+        parts = filename.split("/")[-1].split(".")[0].split("_")
+        chain = parts[0]
+        store = parts[1]
+        with open(filename) as csvfile:
+            reader = iter(csv.reader(csvfile))
+            next(reader)
+            for row in reader:
+                if row[1] == "false":
+                    continue
+                id = row[0]
+                name = row[2]
+                data.add((chain, store, id, name))
+
+for s in data:
+    print(s[0], s[1], s[2], s[3])
