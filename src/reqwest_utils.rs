78c3985d847c1407ec7e66c4417abe145fdf4720
Olivier Hofman <oli.hofmann@gmail.com>
2023-05-31 23:01:08 +0300

    Add method to get text in reqwest_utils.

--- src/reqwest_utils.rs
+++ src/reqwest_utils.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use reqwest::Client;
 use tokio::sync::Semaphore;
 
-pub async fn post_to_text_with_retries<'a>(
+pub async fn post_to_text_with_retries(
     client: &Client,
     url: &str,
     body: String,
@@ -19,6 +19,21 @@ pub async fn post_to_text_with_retries<'a>(
             .send()
             .await
         {
+            Ok(resp) => match resp.text().await {
+                Ok(text) => {
+                    return Some(text);
+                }
+                Err(_) => continue,
+            },
+            Err(_) => continue,
+        };
+    }
+    None
+}
+
+pub async fn get_to_text_with_retries(url: &str) -> Option<String> {
+    for _ in 0..10 {
+        match reqwest::get(url).await {
             Ok(resp) => match resp.text().await {
                 Ok(text) => {
                     return Some(text);
