682f97828ed1a1c9f79c45b4c133e9f5a8ab8ca6
Olivier Hofman <oli.hofmann@gmail.com>
2023-06-01 14:36:08 +0300

    Scripts updated

--- dev_utils/run_all.sh
+++ dev_utils/run_all.sh
@@ -1,3 +1,4 @@
 RUST_LOG=israel_prices=info cargo run --release -- \
  --save-to-json --save-item-infos-to-json --save-to-sqlite --delete-sqlite --clear-files \
- --fetch-rami-levy-metadata --fetch-shufersal-metadata
+ --fetch-rami-levy-metadata --fetch-shufersal-metadata  --fetch-victory-metadata \
+ --fetch-mega-metadata --fetch-yenot-bitan-metadata
--- dev_utils/test_yenot_bitan_fetch.sh
+++ dev_utils/test_yenot_bitan_fetch.sh
@@ -1,4 +1,4 @@
 RUST_LOG=israel_prices=debug cargo run --release -- \
  --no-download --no-curate --no-build-item-infos \
  --load-from-json --load-item-infos-to-json \
- --fetch-rami-levy-metadata --delete-sqlite # --metadata-fetch-limit 100
+ --fetch-yenot-bitan-metadata --delete-sqlite --metadata-fetch-limit 100
