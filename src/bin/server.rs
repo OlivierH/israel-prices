b4fdbba37fec4398071ec4f97adf92e1c7b75691
Olivier Hofman <oli.hofmann@gmail.com>
2023-06-07 11:57:39 +0300

    Basic web server skeleton

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, Result};
 use askama::Template;
 use axum::{
     extract,
@@ -6,32 +7,91 @@ use axum::{
     routing::get,
     Router,
 };
+use rusqlite::Connection;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_templates=debug".into()),
-        )
+        .with(tracing_subscriber::EnvFilter::new("server=debug"))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    // build our application with some routes
-    let app = Router::new().route("/greet/:name", get(greet));
+    let app = Router::new()
+        .route("/greet/:name", get(greet))
+        .route("/", get(index))
+        .route("/stores", get(stores));
 
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    tracing::debug!("listening on http://0.0.0.0:3000");
+    let x = axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
         .await
         .unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+}
+
+fn connection() -> Result<Connection> {
+    let path = "data.sqlite";
+    Ok(rusqlite::Connection::open(path)?)
+}
+
+async fn index() -> Result<impl IntoResponse, AppError> {
+    Ok(Html(
+        std::fs::read_to_string("templates/index.html").unwrap_or("Error".to_string()),
+    ))
+}
+async fn stores() -> Result<impl IntoResponse, AppError> {
+    let connection = connection()?;
+    let mut stmt = connection.prepare("SELECT * FROM Stores")?;
+    let mut result = stmt.query(())?;
+
+    while let Some(value) = result.next()? {
+        print!("{:?}", &value);
+    }
+
+    // .map(|r| {
+    //     let a: i64 = r.get(0;)
+    //     a
+    // });
+    // println!("{:?}", x);
+    // stmt.query_map([], |row| {
+    //     println!("{:?}", row);
+    //     Ok(())
+    // })?;
+
+    Ok(Html(
+        std::fs::read_to_string("templates/index.html").unwrap_or("Error".to_string()),
+    ))
 }
 
 async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
     let template = HelloTemplate { name };
     HtmlTemplate(template)
+}
+
+/* Error handling magic */
+// Make our own error that wraps `anyhow::Error`.
+struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
 
 #[derive(Template)]
